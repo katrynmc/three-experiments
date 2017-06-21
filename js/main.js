64cc145249bb090e24e13767c57a903ef18f3fde
Katryn McIntosh <katrynmcintosh@swipely.com>
2017-06-20 23:43:40 -0400

    Pull out styles into their own file

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-var THREE = require('three');
+const THREE = require('three');
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 55, window.innerWidth/window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 55, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh( geometry, material );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh( geometry, material );
 
 scene.add( cube );
 camera.position.z = 5;
 
-var render = function render() {
+const render = function render() {
   requestAnimationFrame( render );
   cube.rotation.x += 0.051;
   cube.rotation.y += 0.051;
