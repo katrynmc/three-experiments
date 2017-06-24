18b4df299cd4e21d97db61c5664e4f00625605b2
Katryn McIntosh <katrynmcintosh@swipely.com>
2017-06-24 16:28:15 -0400

    Part 1 of Rachel Smith tutorial on Codepen

--- build/main.js
+++ build/main.js
@@ -1,36 +1,61 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 const THREE = require('three');
-// https://threejs.org/docs/index.html#manual/introduction/Drawing-lines
+// https://codepen.io/rachsmith/post/beginning-with-3d-webgl-pt-1-the-scene
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth/window.innerHeight,
-  1,
-  500
-);
-camera.position.set(0, 0, 100);
-camera.lookAt(new THREE.Vector3(0, 0, 0));
+// A scene is the 3D space in which you can place objects, cameras and lighting. The space can be as small or large as you need it to be.
 
 const scene = new THREE.Scene();
 
-const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+// fov: The vertical field of view. This dictates the size of the vertical space your camera's view can reach.
+// aspect: This is the aspect ratio you use to create the horizontal field of view based off the vertical.
+// near: This is the nearest plane of view (where the camera's view begins).
+// far: This is the far plane of view (where the camera's view ends).
 
-// it's recommended to use a BufferGeometry as it's more performant, however for simplicity we'll use a Geometry here
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth/window.innerHeight,
+  0.1,
+  1000
+);
 
-const geometry = new THREE.Geometry();
-geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
-geometry.vertices.push(new THREE.Vector3(0, 10, 0));
-geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+camera.position.z = 100;
 
-const line = new THREE.Line(geometry, material);
+const renderer = new THREE.WebGLRenderer();
 
-scene.add(line);
+renderer.setSize( window.innerWidth, window.innerHeight );
 
-renderer.render(scene, camera);
+document.body.appendChild( renderer.domElement );
+
+// Directional Lights: a large light from far away that shines from one direction (think of the sun).
+// Ambient Lights: less of a light source and more of a soft color tint for the scene.
+// Point Lights: think of a lightbulb - shines in every direction & has a limited range.
+// Spot Lights: just like a spotlight works in real life.
+// Hemisphere Lights: an ambient (non directional) light coming from the ceiling and floor of the scene.
+
+const geometry = new THREE.BoxGeometry(20, 20, 20);
+
+const material = new THREE.MeshLambertMaterial({color: 0xfd59d7});
+
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+const light = new THREE.PointLight(0xFFFF00);
+
+light.position.set(10, 0, 25);
+
+scene.add(light);
+
+const render = function () {
+  requestAnimationFrame( render );
+
+  cube.rotation.x += 0.1;
+  cube.rotation.y += 0.1;
+  camera.updateProjectionMatrix();
+
+  renderer.render(scene, camera);
+};
+render();
 
 },{"three":2}],2:[function(require,module,exports){
 (function (global, factory) {
---
+++ js/tutorial_rachsmith.js
@@ -0,0 +1,57 @@
+const THREE = require('three');
+// https://codepen.io/rachsmith/post/beginning-with-3d-webgl-pt-1-the-scene
+
+// A scene is the 3D space in which you can place objects, cameras and lighting. The space can be as small or large as you need it to be.
+
+const scene = new THREE.Scene();
+
+// fov: The vertical field of view. This dictates the size of the vertical space your camera's view can reach.
+// aspect: This is the aspect ratio you use to create the horizontal field of view based off the vertical.
+// near: This is the nearest plane of view (where the camera's view begins).
+// far: This is the far plane of view (where the camera's view ends).
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth/window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 100;
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+document.body.appendChild( renderer.domElement );
+
+// Directional Lights: a large light from far away that shines from one direction (think of the sun).
+// Ambient Lights: less of a light source and more of a soft color tint for the scene.
+// Point Lights: think of a lightbulb - shines in every direction & has a limited range.
+// Spot Lights: just like a spotlight works in real life.
+// Hemisphere Lights: an ambient (non directional) light coming from the ceiling and floor of the scene.
+
+const geometry = new THREE.BoxGeometry(20, 20, 20);
+
+const material = new THREE.MeshLambertMaterial({color: 0xfd59d7});
+
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+const light = new THREE.PointLight(0xFFFF00);
+
+light.position.set(10, 0, 25);
+
+scene.add(light);
+
+const render = function () {
+  requestAnimationFrame( render );
+
+  cube.rotation.x += 0.1;
+  cube.rotation.y += 0.1;
+  camera.updateProjectionMatrix();
+
+  renderer.render(scene, camera);
+};
+render();
